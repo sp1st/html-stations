ba691dc325efe15d31511a27fb59a05bc6c94731
28oso <28osoblanco2020@gmail.com>
2024-08-19 05:12:25 +0900

    clear: station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,23 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+  const result = await test(userList)
   return await result
 }
 
-function test() {
-  return
+function test(user) {
+  return new Promise((resolve) => {
+    // 3秒後に実行する処理
+    setTimeout(() => {
+      user.map(buildFullName);
+      // 無事処理が終わったことを伝える
+      resolve(user);
+    }, 3000)
+  });
 }
 
+function buildFullName(data) {
+  data.full_name = data.family_name + " " + data.first_name
+  // data.full_name = `${data.family_name} ${data.first_name}`
+  return data
+}
